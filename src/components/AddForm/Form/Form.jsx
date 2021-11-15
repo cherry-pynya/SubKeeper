f98e1954afaab6cdea1d1f84ee3c67e8ea0825fc
chery-pynya <nikita.cherepnya@gmail.com>
2021-11-15 22:41:21 +0300

    datepicker added

--- src/components/AddForm/Form/Form.jsx
+++ src/components/AddForm/Form/Form.jsx
@@ -1,4 +1,10 @@
 import { useState } from "react";
+import DatePicker from '@material-ui/lab/DatePicker';
+import TextField from "@mui/material/TextField";
+import LocalizationProvider from "@material-ui/lab/LocalizationProvider/LocalizationProvider";
+import AdapterDateFns from '@material-ui/lab/AdapterDateFns';
+import ruLocale from 'date-fns/locale/ru';
+import moment from "moment";
 
 export default function Form() {
   const [data, setData] = useState({
@@ -6,12 +12,25 @@ export default function Form() {
     cost: "",
     currency: "",
     option: "",
-    date: "",
+    date: new Date(),
   });
+
+  const handleChange = (e) => {
+    const { name } = e.target;
+    const value = e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+    setData({...data, [name]: value});
+    console.log(data)
+  };
+
+  const setDate = (newDate) => {
+    setData({...data, date: newDate});
+    console.log(data);
+  };
+
   return (
     <form className="add-form">
       <div className="mb-3">
-        <label htmlfor="name" className="form-label">
+        <label htmlFor="name" className="form-label">
           Название:
         </label>
         <input
@@ -20,10 +39,11 @@ export default function Form() {
           id="name"
           name="name"
           value={data.name}
+          onChange={handleChange}
         />
       </div>
       <div className="mb-3">
-        <label htmlfor="cost" className="form-label">
+        <label htmlFor="cost" className="form-label">
           Стоимость:
         </label>
         <input
@@ -32,24 +52,37 @@ export default function Form() {
           id="cost"
           name="cost"
           value={data.cost}
+          onChange={handleChange}
         />
       </div>
       <div className="mb-3">
-        <label htmlfor="currency" className="form-label col" style={{padding: 0}}>
+        <label
+          htmlFor="currency"
+          className="form-label col"
+          style={{ padding: 0 }}
+        >
           Выберите валюту:
         </label>
-        <select className="form-select" id='currency' name='currency'>
-          <option value="RUB" selected>Рубли</option>
+        <select className="form-select" id="currency" name="currency" onChange={handleChange}>
+          <option value="RUB" defaultValue>
+            Рубли
+          </option>
           <option value="USD">Долары</option>
           <option value="EUR">Евро</option>
         </select>
       </div>
       <div className="mb-3">
-        <label htmlfor="currency" className="form-label col" style={{padding: 0}}>
+        <label
+          htmlFor="currency"
+          className="form-label col"
+          style={{ padding: 0 }}
+        >
           Выберите опцию списания:
         </label>
-        <select className="form-select" id='option' name='option'>
-          <option value="1" selected>Раз в месяц</option>
+        <select className="form-select" id="option" name="option" onChange={handleChange}>
+          <option value="1" defaultValue>
+            Раз в месяц
+          </option>
           <option value="2">Раз в два месяца</option>
           <option value="3">Раз в три месяца</option>
           <option value="4">Раз в четыре иксяца</option>
@@ -62,6 +95,26 @@ export default function Form() {
           <option value="11">Раз в одинадцать месяцев</option>
           <option value="12">Раз в год</option>
         </select>
+      </div>
+      <div className="mb-3" style={{marginTop: '40px'}}> 
+        <LocalizationProvider dateAdapter={AdapterDateFns} locale={ruLocale}>
+          <DatePicker
+            disableFuture
+            label="Дата начала подписки"
+            openTo="year"
+            views={["year", "month", "day"]}
+            mask={'__.__.____'}
+            value={data.date}
+            onChange={(newValue) => {
+                setDate(newValue)
+            }}
+            renderInput={(params) => <TextField {...params} />}
+          />
+        </LocalizationProvider>
+      </div>
+      <div className='row mb-3'>
+          <button type='submit' className='btn btn-lg btn-success' style={{margin: '0 20px 0 1rem'}}>Сохранить</button>
+          <button type='button' className='btn btn-danger'>Отмена</button>
       </div>
     </form>
   );
